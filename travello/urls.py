269f2ee97c36e9e6ddeb089ff7964d1a72e1bc98
fidel chavez <fchavezcar@unsa.edu.pe>
2020-07-09 03:37:37 -0500

    formulario para crear un destino

--- travello/urls.py
+++ travello/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 urlpatterns= [
     path('', views.index, name='index'),
-    path('listado', views.listado, name='listado')
+    path('listado', views.listado, name='listado'),
+    path('crear', views.crear, name='crear')
 ]
--- travello/views.py
+++ travello/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Destination
+from .forms import DestinationForm
 # Create your views here.
 def index(request):
     dests = Destination.objects.all()
@@ -10,3 +11,18 @@ def listado(request):
         'destinos':destinos
     }
     return render(request,'listado.html',contexto)
+def crear(request):
+    if request.method=='GET':
+        form = DestinationForm()
+        contexto = {
+            'form':form
+        }
+    else:
+        form = DestinationForm(request.POST)
+        contexto = {
+            'form':form
+        }
+        if form.is_valid():
+            form.save()
+            return redirect('listado')
+    return render(request,'crear.html',contexto)
