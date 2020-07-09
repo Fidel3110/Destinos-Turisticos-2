165d6421ceed181f49616e67778630044683f6ad
fidel chavez <fchavezcar@unsa.edu.pe>
2020-07-08 19:33:39 -0500

    agregue al menu destinos

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ urlpatterns= [
     path('register', views.register, name='register'),
     path('login', views.login, name= 'login'),
     path('logout', views.logout, name= 'logout'),
-    path('contact', views.contact, name= 'contact')
+    path('destino', views.destino, name='destino')
 ]
