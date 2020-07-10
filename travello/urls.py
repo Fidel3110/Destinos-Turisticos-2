7180adf3782b6e680a2adfe5c72bc62aae0d5d5a
fidel chavez <fchavezcar@unsa.edu.pe>
2020-07-09 22:29:57 -0500

    Proyecto terminado insertar editar eliminar y listar

--- travello/urls.py
+++ travello/urls.py
@@ -3,5 +3,7 @@ from .import views
 urlpatterns= [
     path('', views.index, name='index'),
     path('listado', views.listado, name='listado'),
-    path('crear', views.crear, name='crear')
+    path('crear', views.crear, name='crear'),
+    path('editar/<int:id>/', views.editar, name='editar'),
+    path('eliminar/<int:id>/', views.eliminar, name='eliminar')
 ]
--- travello/views.py
+++ travello/views.py
@@ -3,26 +3,50 @@ from .models import Destination
 from .forms import DestinationForm
 # Create your views here.
 def index(request):
-    dests = Destination.objects.all()
-    return render(request,'index.html',{'dests':dests})
+    dest = Destination.objects.all()
+    return render(request, 'index.html', {'dest': dest})
+
 def listado(request):
     destinos = Destination.objects.all()
-    contexto  = {
-        'destinos':destinos
+    contexto = {
+    'destinos':destinos
     }
-    return render(request,'listado.html',contexto)
+    return render(request,'listado.html', contexto)
+
 def crear(request):
-    if request.method=='GET':
+    if request.method == 'GET':
         form = DestinationForm()
-        contexto = {
+        contexto ={
             'form':form
         }
     else:
-        form = DestinationForm(request.POST)
-        contexto = {
+        form = DestinationForm(request.POST, request.FILES)
+        contexto ={
             'form':form
         }
         if form.is_valid():
             form.save()
             return redirect('listado')
-    return render(request,'crear.html',contexto)
+    return render(request,'crear.html', contexto)
+def editar(request,id):
+    destinos = Destination.objects.get(id=id)
+    if request.method == 'GET':
+        form = DestinationForm(instance = destinos)
+        contexto ={
+        'form':form
+        }
+    else:
+        form = DestinationForm(request.POST, instance = destinos)
+        contexto ={
+        'form':form
+        }
+        if form.is_valid():
+            form.save()
+            return redirect('listado')
+    return render(request, 'crear.html', contexto)
+
+def eliminar(request,id):
+    destinos = Destination.objects.get(id=id)
+    destinos.delete()
+
+    return redirect('listado')
